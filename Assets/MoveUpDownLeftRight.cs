f187e273dac0317aef74f150256fa378705457cd
Brad Dunn <brad@braddunn.com>
2014-08-08 12:34:45 -0400

    Initial Project Assets

---
+++ Assets/MoveUpDownLeftRight.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveUpDownLeftRight : MonoBehaviour {
+
+	public float speed = 10.0f;
+
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.position += Vector3.right * Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+		transform.position += Vector3.up * Input.GetAxis ("Vertical") * Time.deltaTime * speed;
+	}
+}
